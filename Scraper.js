4b0f850b9973d9a1937b8631f7720f05b5673fd3
unknown <grdavies99@gmail.com>
2020-10-26 12:31:25 -0600

    scraper can now handle multiple inputs

--- Scraper.js
+++ Scraper.js
@@ -1,24 +1,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-
-const url = 'https://www.newegg.com/crucial-p1-1tb/p/N82E16820156199';
-axios.get(url).then(response=>{
-    // console.log(response.data);
-    let getData = html =>{
-        data = [];
-        const $ = cheerio.load(html);
-        title = $('h1.product-title').text();
-        price =  $('div.product-buy-box li.price-current').text();
-        // .each((i,elem) =>{
-            data.push({
-                title : title,
-                price : price
-            // });
-        });
-        console.log(data);
+const prompt  = require('prompt-sync')({sigint: true});
+url = [];
+url[0] = prompt('please enter the url of your item or q to quit ');
+var count = 1;
+while (url[count] != 'q'){
+    
+    url[count] = prompt('please enter the url of your next item or q to quit ');
+    if(url[count] != "q"){
+        count++;
     }
-    getData(response.data);
-}).catch(error=>{
-    console.log(error);
-})
+}
+url.forEach(element => {
+    if(element != 'q'){
+        axios.get(element).then(response=>{
+            // console.log(response.data);
+            let getData = html =>{
+                data = [];
+                const $ = cheerio.load(html);
+                title = $('h1.product-title').text();
+                price =  $('div.product-buy-box li.price-current').text();
+                    data.push({
+                        title : title,
+                        price : price
+                });
+                console.log(data);
+            }
+            getData(response.data);
+        }).catch(error=>{
+            console.log(error);
+        })
+    }
+});
+
+
 
