15552d277bb5eaddc5b51cf69381d105f9d63cbc
unknown <grdavies99@gmail.com>
2020-10-29 21:31:34 -0600

    added amazon search functionality

---
+++ AmazonScraper.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const prompt  = require('prompt-sync')({sigint: true});
+url = [];
+url[0] = prompt('please enter the url of your item or q to quit ');
+var count = 1;
+while (url[count] != 'q'){
+    
+    url[count] = prompt('please enter the url of your next item or q to quit ');
+    if(url[count] != "q"){
+        count++;
+    }
+}
+url.forEach(element => {
+    if(element != "q"){
+        axios.get(element).then(Response =>{
+            let getData = html=>{
+                data = [];
+                const $ = cheerio.load(html);
+                title = $('h1#title span#productTitle').text().replace(/(\r\n|\n|\r)/gm,"");
+                price = $('span#price_inside_buybox').text().replace(/(\r\n|\n|\r)/gm,"");
+                data.push({
+                    title : title,
+                    price : price
+                });
+                console.log(data);
+            }
+            getData(Response.data);
+        }).catch(error=>{
+            console.log(error);
+        })
+    }
+});
--- HelloNode.js
+++ HelloNode.js
@@ -3,10 +3,12 @@ const port = 3000;
 
 const server = http.createServer((req,res)=>{
     res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('<h1>Hello World</h1>');
+    res.setHeader('Content-Type','text/html');
+    res.end('<h1>Hello World</h1><br><button id="clicker">Press</button>');
 });
 
 server.listen(port, ()=>{
+   
     console.log('server running on port: '+port);
+    
 });
